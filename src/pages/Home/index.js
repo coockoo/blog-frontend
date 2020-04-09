a5e34b306e2ac0721b17e85c1216806452e04ee3
Oleksandr Bondarchuk <alex.coockoo@gmail.com>
2020-04-09 23:02:59 +0300

    Add initial HomePage reducer

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,30 @@
-import React from 'react';
+import React, { useEffect, useReducer } from 'react';
+
+import graphQL from '../../services/graphql';
+import articlesQuery from '../../services/graphql/queries/articles.gql';
+
+import { at, reducer, initialState } from './reducer';
+
+import s from './styles.less';
+
+async function loadArticles(dispatch) {
+  dispatch({ type: at.LOAD_ARTICLES_START });
+  try {
+    const res = await graphQL(articlesQuery);
+    const { count, rows } = res.data.articles;
+    dispatch({ type: at.LOAD_ARTICLES_SUCCESS, count, rows });
+  } catch (error) {
+    console.error(error);
+    dispatch({ type: at.LOAD_ARTICLES_ERROR });
+  }
+}
 
 export default function HomePage() {
-  return <div>Kappa</div>;
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    loadArticles(dispatch);
+  }, []);
+
+  return <div className={s.homePage}>{state.rows.map((article) => article.id)}</div>;
 }
---
+++ src/pages/Home/reducer.js
@@ -0,0 +1,30 @@
+import createReducer from '../../utils/createReducer';
+
+export const at = {
+  LOAD_ARTICLES_START: 'LOAD_ARTICLES_START',
+  LOAD_ARTICLES_SUCCESS: 'LOAD_ARTICLES_SUCCESS',
+  LOAD_ARTICLES_ERROR: 'LOAD_ARTICLES_ERROR',
+};
+
+export const initialState = {
+  isLoading: true,
+  rows: [],
+  count: 0,
+};
+
+export const reducer = createReducer({
+  [at.LOAD_ARTICLES_START]: (state) => ({
+    ...state,
+    isLoading: true,
+  }),
+  [at.LOAD_ARTICLES_SUCCESS]: (state, action) => ({
+    ...state,
+    count: action.count,
+    rows: action.rows,
+    isLoading: false,
+  }),
+  [at.LOAD_ARTICLES_ERROR]: (state) => ({
+    ...state,
+    isLoading: false,
+  }),
+});
