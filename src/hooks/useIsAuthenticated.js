641823817845a9f9e70b0077ebdbc048a7fa1fec
Oleksandr Bondarchuk <alex.coockoo@gmail.com>
2020-04-25 08:36:58 +0300

    Add useIsAuthenticated hook

---
+++ src/hooks/useIsAuthenticated.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react';
+
+import auth from '../services/auth';
+
+export default function useIsAuthenticated() {
+  const [isAuthenticated, setAuthenticated] = useState(auth.isAuthenticated());
+
+  useEffect(() => {
+    function authSubscription() {
+      setAuthenticated(auth.isAuthenticated());
+    }
+
+    auth.subscribe(authSubscription);
+    return () => {
+      auth.unsubscribe(authSubscription);
+    };
+  }, []);
+
+  return isAuthenticated;
+}
