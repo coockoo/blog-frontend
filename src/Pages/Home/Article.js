c50b7b58814752915f32d75af3266c0ed314c589
Oleksandr Bondarchuk <alex.coockoo@gmail.com>
2020-05-01 12:26:46 +0300

    Add prettier date formatting for articles

--- src/Pages/Home/Article.js
+++ src/Pages/Home/Article.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { parseISO, format } from 'date-fns';
 
 import s from './styles.less';
 
 export default function Article(props) {
-  const dateStr = new Date(+props.createdAt).toISOString();
-
   return (
     <div className={s.article}>
       <h1>
         <Link to={`/articles/${props.id}`}>{props.title}</Link>
       </h1>
-      <div className={s.date} title={dateStr}>
-        {dateStr}
+      <div className={s.date} title={props.createdAt}>
+        {format(parseISO(props.createdAt), "eeee, do 'of' MMMM yyyy")}
       </div>
       <p className={s.outline}>{props.outline}</p>
     </div>
