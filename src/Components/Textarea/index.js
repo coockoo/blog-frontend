045ec0d1ff03a7c45ed5fde5b51e76a45349179c
Oleksandr Bondarchuk <alex.coockoo@gmail.com>
2020-05-04 17:03:21 +0300

    Add resize solution to textarea component

--- src/Components/Textarea/index.js
+++ src/Components/Textarea/index.js
@@ -5,8 +5,12 @@ import s from './styles.less';
 
 export default function Textarea(props) {
   const { className, onChange, value, ...rest } = props;
+  const rows = value.split(/\n/).length;
+  const minRows = 10;
+
   return (
     <textarea
+      rows={Math.max(rows, minRows)}
       value={value || ''}
       onChange={(e) => onChange(e.target.value)}
       className={cn(className, s.textarea)}
