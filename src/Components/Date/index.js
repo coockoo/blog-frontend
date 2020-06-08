d3b95b60505bf2505d686441f9b534512cfb9bb4
Oleksandr Bondarchuk <alex.coockoo@gmail.com>
2020-06-08 19:30:11 +0300

    Add isLoading state to Date and Title Components (not sure now if it's ok)

--- src/Components/Date/index.js
+++ src/Components/Date/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import { parseISO, format } from 'date-fns';
+import { parseISO, format as formatFn } from 'date-fns';
+import cn from 'classnames';
 
 import s from './styles.less';
 
 export default function Date(props) {
+  const { value, format, className, isLoading } = props;
+
+  const formattedValue = value ? formatFn(parseISO(value), format) : null;
+  const content = isLoading ? null : formattedValue;
+
   return (
-    <div className={s.date} title={props.value}>
-      {format(parseISO(props.value), "eeee, do 'of' MMMM yyyy")}
+    <div className={cn(s.date, { [s.isLoading]: isLoading }, className)} title={value}>
+      {content}
     </div>
   );
 }
+
+Date.defaultProps = {
+  format: "eeee, do 'of' MMMM yyyy",
+};
--- src/Components/Title/index.js
+++ src/Components/Title/index.js
@@ -4,10 +4,13 @@ import cn from 'classnames';
 import s from './styles.less';
 
 export default function Title(props) {
-  const { children, className, ...rest } = props;
+  const { children, className, isLoading, ...rest } = props;
+
+  const content = isLoading ? null : children;
+
   return (
-    <h1 className={cn(s.title, className)} {...rest}>
-      {children}
+    <h1 className={cn(s.title, { [s.isLoading]: isLoading }, className)} {...rest}>
+      {content}
     </h1>
   );
 }
